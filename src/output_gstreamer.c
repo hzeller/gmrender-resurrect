7a469dea67628a56285b08eb29d9651673683470
Henner Zeller <h.zeller@acm.org>
2012-10-23 23:56:44 -0700

    o don't restrict to audio/

--- src/output_gstreamer.c
+++ src/output_gstreamer.c
@@ -55,14 +55,10 @@ static void scan_caps(const GstCaps * caps)
 		return;
 	}
 
-	static const char mime_prefix[] = "audio/";
-	static int mime_prefix_len = sizeof(mime_prefix) - 1;
 	for (i = 0; i < gst_caps_get_size(caps); i++) {
 		GstStructure *structure = gst_caps_get_structure(caps, i);
 		const char *mime_type = gst_structure_get_name(structure);
-		if (strncmp(mime_type, mime_prefix, mime_prefix_len) == 0) {
-			register_mime_type(mime_type);
-		}
+		register_mime_type(mime_type);
 	}
 
 }
