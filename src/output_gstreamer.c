e520cc14189286a9851b8fea6ea1d67b290b69f7
Tucker Kern <tuckkern@gmail.com>
2021-01-01 18:25:12 -0700

    Update the playbin's URI when set_uri is called and we are currently playing something

--- src/output_gstreamer.c
+++ src/output_gstreamer.c
@@ -156,6 +156,10 @@ static void output_gstreamer_set_uri(const char *uri,
 	gsuri_ = (uri && *uri) ? strdup(uri) : NULL;
 	meta_update_callback_ = meta_cb;
 	SongMetaData_clear(&song_meta_);
+
+	// If already playing, update the playbin's URI
+	if (get_current_player_state() == GST_STATE_PLAYING)
+		output_gstreamer_play(play_trans_callback_);
 }
 
 static int output_gstreamer_play(output_transition_cb_t callback) {
